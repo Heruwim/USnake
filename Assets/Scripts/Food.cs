1ab3a72aacf83d0884898a29bc3debf2697b7dcd
Heruwim <heruwim25@gmail.com>
2023-03-02 14:12:01 +0200

    Added random food creation

---
+++ Assets/Scripts/Food.cs
@@ -0,0 +1,26 @@
+using UnityEngine;
+
+public class Food : MonoBehaviour
+{
+    [SerializeField] private BoxCollider2D _gridArea;
+
+    private void Start()
+    {
+        RandomaizePosition();
+    }
+
+    private void RandomaizePosition()
+    {
+        Bounds bounds = _gridArea.bounds;
+        float x = Random.Range(bounds.min.x, bounds.max.x); 
+        float y = Random.Range(bounds.min.y, bounds.max.y);
+        
+        this.transform.position = new Vector3(Mathf.Round(x), Mathf.Round(y), 0.0f);
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if(other.tag == ("Player"))
+        RandomaizePosition();
+    }
+}
